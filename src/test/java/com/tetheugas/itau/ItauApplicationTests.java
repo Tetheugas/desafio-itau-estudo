4956d8bc1053c94d2ff526e930de888d3fafc168
Matheus <mc75071@gmail.com>
2025-03-05 19:33:17 -0300

    aplicação desafio itau estudo

---
+++ src/test/java/com/tetheugas/itau/ItauApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tetheugas.itau;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItauApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
